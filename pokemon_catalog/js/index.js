3c52021bcf36c111c19159a2f2284be6c7ff9c35
Marcin Kebłesz <marcin.keblesz@fingo.pl>
2020-09-08 11:25:37 +0200

    final version

--- pokemon_catalog/js/index.js
+++ pokemon_catalog/js/index.js
@@ -1,37 +1,63 @@
 class PokemonTCGCatalog {
   constructor() {
     this.pageSize = 4;
-    this.currentPage = 2;
+    this.currentPage = 1;
     this.cards = [];
+    this.newCards = [];
 
-    this.catalog = 'null';
-    this.button = 'null';
+    this.catalog = null;
+    this.button = null;
+    this.loader = null;
+    this.search = null;
+    this.info = null;
 
-    this.API = 'https://api.pokemontcg.io';
-    this.API_VERSION = 'v1';
-    this.API_RESOURCE = 'cards';
+    this.API = "https://api.pokemontcg.io";
+    this.API_VERSION = "v1";
+    this.API_RESOURCE = "cards";
     this.API_ENDPOINT = `${this.API}/${this.API_VERSION}/${this.API_RESOURCE}`;
 
     this.UiSelectors = {
-      content: '[data-content]',
-      button: '[data-button]',
+      content: "[data-content]",
+      button: "[data-button]",
+      loader: "[data-loader]",
+      search: "search",
+      card: "[data-card]",
+      info: "[data-info]",
     };
   }
 
-  async initializeCatalog() {
+  initializeCatalog() {
     this.catalog = document.querySelector(this.UiSelectors.content);
     this.button = document.querySelector(this.UiSelectors.button);
+    this.loader = document.querySelector(this.UiSelectors.loader);
+    this.search = document.getElementById(this.UiSelectors.search);
+    this.info = document.querySelector(this.UiSelectors.info);
+
+    this.addEventListeners();
     this.pullCards();
   }
 
   async pullCards() {
+    this.toggleShowElement(this.loader, this.button);
     const { cards } = await this.fetchData(
       `${this.API_ENDPOINT}?page=${this.currentPage}&pageSize=${this.pageSize}`
     );
+    this.toggleShowElement(this.loader, this.button);
+    this.cards = [...this.cards, ...cards];
 
-    this.cards = [...cards];
+    this.newCards = [...cards];
 
-    this.createCatalog(this.cards);
+    this.createCatalog(this.newCards);
+    this.currentPage++;
+  }
+
+  toggleShowElement(...elements) {
+    elements.forEach((element) => element.classList.toggle("hide"));
+  }
+
+  addEventListeners() {
+    this.button.addEventListener("click", () => this.pullCards());
+    this.search.addEventListener("keyup", () => this.filterCards());
   }
 
   async fetchData(url) {
@@ -42,23 +68,53 @@ class PokemonTCGCatalog {
   }
 
   createCatalog(cards) {
-    this.catalog.innerHTML += [
-      cards.map((card) => this.createCard(card)).join(''),
-    ];
+    this.catalog.insertAdjacentHTML("beforeend", [
+      cards.map((card) => this.createCard(card)).join(""),
+    ]);
   }
 
-  createCard({ name, number, imageUrl, supertype, subtype, rarity }) {
+  createCard({ name, number, imageUrl, supertype, subtype, rarity, id }) {
     return `
-        <article class="card">
-            <header class="card__header">
-                <h2 class="card__heading">${name}</h2>
-                <p class="card__number">Nr: ${number}</p>
-                <img class="card__image" src="${imageUrl}" alt="${name}">
-                <p class="card__description"><span class="bold">Supertype:</span> ${supertype}</p>
-                <p class="card__description"><span class="bold">Subtype:</span> ${subtype}</p>
-                <p class="card__description"><span class="bold">Rarity:</span> ${rarity}</p>
-            </header>
-        </article>
-      `;
+      <article class="card" id=${id} data-card>
+        <header class="card__header">
+          <h2 class="card__heading">
+            ${name}
+          </h2>
+          <p class="card__number">Nr: ${number}</p>
+        </header>
+        <img class="card__image" src="${imageUrl}" alt="${name}">
+        <p class="card__description"><span class="bold">Supertype:</span> ${supertype}</p>
+        <p class="card__description ${
+          subtype ? "" : "hide"
+        }"><span class="bold">Subtype:</span> ${subtype}</p>
+        <p class="card__description ${
+          rarity ? "" : "hide"
+        }"><span class="bold">Rarity:</span> ${rarity}</p>
+      </article>
+    `;
+  }
+
+  filterCards() {
+    const searchQuery = this.search.value.toLowerCase();
+
+    searchQuery.length
+      ? this.button.classList.add("hide")
+      : this.button.classList.remove("hide");
+
+    document
+      .querySelectorAll(this.UiSelectors.card)
+      .forEach((element) => element.classList.remove("hide"));
+
+    const filteredCards = this.cards.filter(
+      ({ name }) => !name.toLowerCase().includes(searchQuery)
+    );
+
+    filteredCards.length === this.cards.length
+      ? this.info.classList.remove("hide")
+      : this.info.classList.add("hide");
+
+    filteredCards.forEach(({ id }) =>
+      document.getElementById(id).classList.add("hide")
+    );
   }
 }
